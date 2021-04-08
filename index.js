0dc4f8e8d97f8fc6e286ebe3924d13eeb0f124ae
Randy Ortiz <Randyortiz28@gmail.com>
2021-04-08 18:06:59 -0400

    changes to files

---
+++ index.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+const dotenv = require('dotenv').config()
+const server = require('./api/server.js')
+const express = require('express')
+
+const path = require('path')
+const port = process.env.PORT
+
+server.use(express.static(path.join(__dirname, 'client/build')))
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
